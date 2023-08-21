5dfdbeae2c6627e2c599a60d6ec75601c683e7f2
Bright Zheng <bright.zheng@outlook.com>
2023-08-21 20:45:05 +0800

    add a simple build script

---
+++ example-app/build.sh
@@ -0,0 +1,4 @@
+GOOS=darwin GOARCH=arm64 go build -o example-app-darwin-arm64
+GOOS=darwin GOARCH=amd64 go build -o example-app-darwin-amd64
+GOOS=linux GOARCH=arm64  go build -o example-app-linux-arm64
+GOOS=linux GOARCH=amd64  go build -o example-app-linux-amd64
